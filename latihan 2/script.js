b79cea24addb52a9a6fc7a50bbe102df15b0401a
Faldiii15 <rifaldif888@gmail.com>
2024-03-28 14:31:47 +0700

    Update
    Kalkulator dan List Data Mahasiswa

---
+++ latihan 2/script.js
@@ -0,0 +1,9 @@
+let data = [Faldi,Rismanto,Danang];
+let listMhs = document.getElementById("lstmhs");
+
+data.forEach(showMahasiswa;
+
+    function showMahasiswa(value, index) {
+        console.log(value);
+    // listMhs.innerHTML += "<li>"
+    }
